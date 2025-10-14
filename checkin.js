635c177b86ef7e3b3189a8b661b3ef25e5af9e98
Omegatagram <chetniphat.rat@gmail.com>
2025-10-14 21:43:42 +0700

    Update checkin.js

--- checkin.js
+++ checkin.js
@@ -12,12 +12,4 @@ document.getElementById("emailPrefix").addEventListener("input", () => {
 
 });
 
-function isInAppBrowser() {
-  const ua = navigator.userAgent || navigator.vendor || window.opera;
-  return /FBAN|FBAV|Instagram|Line/i.test(ua);
-}
-
-if (isInAppBrowser()) {
-  alert("⚠️ For full functionality, please open this link using Chrome or Safari.");
-}
 
